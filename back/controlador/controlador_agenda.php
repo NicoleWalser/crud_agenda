de0efa1ac4cb9b182d7ee1c056d011eb62118667
NicoleWalser <IAEnicole05@gmail.com>
2024-07-21 19:45:26 -0300

    terminadooo

--- back/controlador/controlador_agenda.php
+++ back/controlador/controlador_agenda.php
@@ -3,7 +3,57 @@ header("Access-Control-Allow-Origin:*");
 
 require_once '../modelo/modelo_agenda.php';
 
-$funcion = $_GET['funcion'];
+
+
+
+$request = $_GET['request'];
+switch ($request){
+    case "agregaragenda":
+        agregaragenda();
+        break;
+    case "obteneragenda":
+        obteneragenda();
+        break;
+    case "eliminaragenda":
+        eliminaragenda();
+        break;
+    case "modificaragenda":
+        modificaragenda();
+        break;
+}
+
+function agregaragenda(){
+    $nombre = $_POST['nombre'];
+    $fecha = $_POST['fecha'];
+    $hora = $_POST['hora'];
+    $estado = $_POST['estado'];
+    $result = (new agenda())->agregaragenda($nombre, $fecha, $hora, $estado);
+    echo json_encode($result);
+}
+
+function obteneragenda(){
+    $result = (new agenda())->obteneragenda();
+    echo json_encode($result);
+}
+  
+function eliminaragenda(){
+    $id = $_POST['id'];
+    $resultado = (new agenda())->eliminaragenda($id);
+    echo json_encode($resultado);
+
+}
+
+function modificaragenda(){
+    $nombre = $_POST['nombre'];
+    $fecha = $_POST['fecha'];
+    $hora = $_POST['hora'];
+    $estado = $_POST['estado'];
+    $resultado = (new agenda())->modificaragenda($nombre,$fecha, $hora, $estado);
+    echo json_encode($resultado);
+}
+
+
+/*$funcion = $_GET['funcion'];
 
 switch ($funcion) {
     case "agregar":
@@ -18,6 +68,7 @@ switch ($funcion) {
     case "obtener":
         obtenerAgenda();
     break;
+
 }
 
 function agregarAgenda(){
@@ -48,5 +99,5 @@ function eliminarAgenda(){
 function obtenerAgenda(){
     $resultado = (new agenda())->obtenerAgenda();   
     echo json_encode($resultado);
-}
+}*/
 ?>
--- back/modelo/modelo_agenda.php
+++ back/modelo/modelo_agenda.php
@@ -1,7 +1,40 @@
 <?php
 require_once  '../conexion/conexion.php';
+class agenda{
 
-class agenda {
+    function obteneragenda(){
+        $connection = connection();
+        $sql = "SELECT * FROM tareas";
+        $respuesta = $connection->query($sql);
+        $tareas = $respuesta->fetch_all(MYSQLI_ASSOC);
+        return $tareas;
+    }
+
+    function agregaragenda($nombre, $fecha, $hora, $estado){
+        $connection = connection();
+        $sql = "INSERT INTO tareas(nombre, fecha, hora, estado) VALUES('$nombre','$fecha', '$hora', '$estado');";
+        $connection->query($sql);
+    }
+    function eliminaragenda($id){
+        $connection = connection();
+        $sql = "DELETE FROM tareas WHERE (id ='$id');";
+        $respuesta = $connection->query($sql);
+        return $respuesta;
+    }
+    
+    public function modificaragenda($nombre, $estado, $fecha, $hora){
+        $sql = "UPDATE tareas SET nombre='$nombre', fecha='$fecha', hora='$hora', estado='$estado'";
+        $connection = connection();
+        $respuesta = $connection->query($sql);
+        return $respuesta;
+
+    }
+}
+
+
+
+
+/*class agenda {
 
     function obtenerAgenda(){
         $connection = connection();
@@ -22,9 +55,8 @@ class agenda {
         catch(Exception $e){
             $error = $e ->getMessage();
             echo $error;
-        }
-        
-        
+        }   
+
     }
   
     function eliminarAgenda($id){
@@ -41,4 +73,5 @@ class agenda {
         return $respuesta;
 
 }
-}
+}*/
+?>
